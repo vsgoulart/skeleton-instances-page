5a8390de893dba5b7fd214e60563b7e6d1a839d2
Vinicius Goulart <goulart.vs@gmail.com>
2020-03-31 21:20:06 +0200

    Implement operations panel

--- src/App/Instances/Operations/index.js
+++ src/App/Instances/Operations/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import classNames from './index.module.scss';
+import {OPERATIONS} from './mocks';
+
 function Operations() {
   return (
-    <div>
+    <div className={classNames.operations}>
       <h2>Operations</h2>
+      {OPERATIONS.map(({id, startDate, endDate, operationsFinishedCount, operationsTotalCount}) => (
+        <div key={id} className={classNames.operation}>
+          <div>{id}</div>
+          <div>{startDate}</div>
+          <div>{endDate || '-'}</div>
+          <div>{`${operationsFinishedCount} / ${operationsTotalCount}`}</div>
+        </div>
+      ))}
     </div>
   );
 }
