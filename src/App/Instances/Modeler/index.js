c32c3c05881c16b7a8df24a4d7a0270b21d0b00f
Vinicius Goulart <goulart.vs@gmail.com>
2020-03-31 14:43:35 +0200

    Create modeler layout

--- src/App/Instances/Modeler/index.js
+++ src/App/Instances/Modeler/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import classNames from './index.module.scss';
+
 function Modeler() {
   return (
-    <div>
+    <div className={classNames.modeler}>
       <h2>Modeler</h2>
     </div>
   );
